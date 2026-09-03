a9a42322de1990ad4c89c10629af55cd13383068
ahmedhossam32 <ahmedhemly2020@yahoo.com>
2026-09-03 15:31:33 +0300

    Initial commit: Spring Boot project skeleton via Spring Initializr

---
+++ src/main/java/com/ahmedhossam/mnp/MnpPortingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ahmedhossam.mnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnpPortingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MnpPortingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahmedhossam/mnp/MnpPortingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmedhossam.mnp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnpPortingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
